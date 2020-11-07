57023c201cdf9e04f8a911753ab87c35115b4d71
Lee murray <leemurray@Lees-MacBook-Pro.local>
2020-11-07 10:03:27 +0000

    first commit

---
+++ src/_data/meta.js
@@ -0,0 +1,8 @@
+module.exports = {
+  url: process.env.URL || "http://localhost:8080",
+  siteName: "Digital Coalface",
+  siteDescription:
+    "Digital in 2020...",
+  authorName: "Lee Murray",
+  twitterUsername: "uxeskimo", // no `@`
+};
